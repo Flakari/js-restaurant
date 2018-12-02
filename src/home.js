1b8788987435ab911fa9d6b5ccc834f4e0449c5a
Nate Dimock <natedimock@yahoo.com>
2018-12-02 12:14:50 -0600

    Broke up home functions, created initial functionality of menu grid

--- src/home.js
+++ src/home.js
@@ -16,30 +16,13 @@ function createHome() {
     contentGrid.appendChild(contentImage);
     contentGrid.appendChild(contentText);
 
-
     content.setAttribute('style', 'background-color: #d0a385;' +
-                                  'overflow: hidden;');
-    
-    contentGrid.setAttribute('style', 'margin: 20px auto;' +
-                                      'background-color: white;' +
-                                      'width: 1000px;' +
-                                      'max-width: 100%;' +
-                                      'display: grid;' +
-                                      'grid-template-columns: minmax(50px, 300px) 1fr;' +
-                                      'align-items: center;' +
-                                      'font-size: 1.1em;');
+                                  'overflow: hidden;' +
+                                  'text-align: center;');
 
-    contentImage.setAttribute('src', 'images/interior2.jpg');
-    contentImage.setAttribute('style', 'width: 100%;');
-
-    contentText.setAttribute('style', 'white-space: pre-wrap;' +
-                                      'font-family: Arial, sans-serif;' +
-                                      'padding: 0 20px;' +
-                                      'text-align: justify;');
-
-    contentText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a mi pellentesque, dapibus velit a, lacinia massa. Vestibulum vehicula leo risus, condimentum blandit nisl suscipit id. Nunc rutrum bibendum urna eu tempor. Cras a dui risus. Phasellus vel consectetur mi, vitae pharetra magna. Duis finibus vehicula justo, sed suscipit libero accumsan eget. Proin in ex ante. Quisque at facilisis eros, sed dictum elit. Praesent commodo nisi et felis ultricies, sed imperdiet lorem suscipit. Morbi justo ex, imperdiet ut purus eget, sollicitudin interdum eros. Maecenas blandit lectus non nibh euismod feugiat. Cras id laoreet ex. Donec lobortis vestibulum augue a eleifend. Pellentesque tempor ac tortor sed viverra.\n\n' +
-
-    'Mauris sit amet volutpat libero, at interdum massa. Nullam sed ipsum ut enim tristique mattis quis ut massa. Sed suscipit orci eu ante faucibus, eu volutpat nulla feugiat. Etiam a risus eu nulla varius pulvinar. Ut in nisi velit. Pellentesque a rhoncus magna. Phasellus tempus, arcu ut aliquam posuere, eros nunc porta sapien, vel luctus erat tortor aliquet risus. Nam auctor fermentum sollicitudin.';
+    styleContentGrid();
+    styleContentImage();
+    styleContentText();
 
     function mainMediaQuery(size) {
         if (size.matches) {
@@ -58,4 +41,37 @@ function createHome() {
     let homeSize = window.matchMedia('(max-width: 920px)');
     mainMediaQuery(homeSize);
     homeSize.addListener(mainMediaQuery);
+}
+
+function styleContentGrid() {
+    let contentGrid = document.querySelector('section');
+
+    contentGrid.setAttribute('style', 'margin: 20px auto;' +
+                                      'background-color: white;' +
+                                      'width: 1000px;' +
+                                      'max-width: 100%;' +
+                                      'display: grid;' +
+                                      'grid-template-columns: minmax(50px, 300px) 1fr;' +
+                                      'align-items: center;' +
+                                      'font-size: 1.1em;');
+}
+
+function styleContentImage() {
+    let contentImage = document.querySelector('img'); 
+
+    contentImage.setAttribute('src', 'images/interior2.jpg');
+    contentImage.setAttribute('style', 'width: 100%;');
+}
+
+function styleContentText() {
+    let contentText = document.querySelector('article');
+
+    contentText.setAttribute('style', 'white-space: pre-wrap;' +
+                                      'font-family: Arial, sans-serif;' +
+                                      'padding: 0 20px;' +
+                                      'text-align: justify;');
+
+    contentText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a mi pellentesque, dapibus velit a, lacinia massa. Vestibulum vehicula leo risus, condimentum blandit nisl suscipit id. Nunc rutrum bibendum urna eu tempor. Cras a dui risus. Phasellus vel consectetur mi, vitae pharetra magna. Duis finibus vehicula justo, sed suscipit libero accumsan eget. Proin in ex ante. Quisque at facilisis eros, sed dictum elit. Praesent commodo nisi et felis ultricies, sed imperdiet lorem suscipit. Morbi justo ex, imperdiet ut purus eget, sollicitudin interdum eros. Maecenas blandit lectus non nibh euismod feugiat. Cras id laoreet ex. Donec lobortis vestibulum augue a eleifend. Pellentesque tempor ac tortor sed viverra.\n\n' +
+
+    'Mauris sit amet volutpat libero, at interdum massa. Nullam sed ipsum ut enim tristique mattis quis ut massa. Sed suscipit orci eu ante faucibus, eu volutpat nulla feugiat. Etiam a risus eu nulla varius pulvinar. Ut in nisi velit. Pellentesque a rhoncus magna. Phasellus tempus, arcu ut aliquam posuere, eros nunc porta sapien, vel luctus erat tortor aliquet risus. Nam auctor fermentum sollicitudin.';
 }
--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,7 @@ export { createMenu };
 function createMenu() {
     let content = document.querySelector('#content');
     let contentGrid = document.createElement('section');
+    let menuHeader = document.createElement('h2');
 
     if (content.hasChildNodes) {
         while (content.firstChild) {
@@ -10,5 +11,27 @@ function createMenu() {
           }
     }
 
+    content.appendChild(menuHeader);
     content.appendChild(contentGrid);
+    
+
+    menuHeader.textContent = 'Menu';
+
+    
+
+    contentGrid.setAttribute('style', 'margin: 20px auto;' +
+                                      'background-color: white;' +
+                                      'width: 1000px;' +
+                                      'max-width: 100%;' +
+                                      'display: grid;' +
+                                      'grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));' +
+                                      'align-items: center;' +
+                                      'font-size: 1.1em;');
+
+
+    for (let i = 0; i < 6; i++) {
+        let menuItem = document.createElement('div');
+        menuItem.textContent = 'Hi';
+        contentGrid.appendChild(menuItem);
+    }
 }
